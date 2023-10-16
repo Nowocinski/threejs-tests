e81a3647c15f2b2e0abc4473ff5d59d8e11a40a2
rafal.nowocinski <vito24222@gmail.com>
2023-10-16 15:45:49 +0200

    Update test2.js

--- EdgesGeometry/test2.js
+++ EdgesGeometry/test2.js
@@ -25,35 +25,6 @@ let angleStep = Math.PI * 0.5;
 let radius = 0.1;
 
 // ---------------
-const checkAngle = (vector1, vector2) => {
-    const directionVector = new THREE.Vector2(vector2.x - vector1.x, vector2.y - vector1.y);
-    const angleRadians = Math.atan2(directionVector.y, directionVector.x);
-    // const angleDegrees = THREE.MathUtils.radToDeg(angleRadians);
-    // return angleDegrees;
-    return angleRadians;
-};
-
-const calculateAngle = (vertex1, vertex2, vertex3) => {
-    // Oblicz wektor między pierwszym a drugim wierzchołkiem
-    const vector1 = new THREE.Vector3().subVectors(vertex1, vertex2);
-
-    // Oblicz wektor między trzecim a drugim wierzchołkiem
-    const vector2 = new THREE.Vector3().subVectors(vertex3, vertex2);
-
-    // Oblicz kąt między tymi dwoma wektorami przy użyciu funkcji Math.atan2
-    let angleRadians = Math.atan2(vector2.y, vector2.x) - Math.atan2(vector1.y, vector1.x);
-
-    // Konwersja kąta z radianów na stopnie
-    let angleDegrees = THREE.MathUtils.radToDeg(angleRadians);
-
-    // Uzyskaj wartość dodatnią kąta, aby mieć wynik między 0 a 360 stopni
-    if (angleDegrees < 0) {
-        angleDegrees += 360;
-    }
-
-    console.log("Miarę kąta między trzema wierzchołkami wynosi: " + angleDegrees + " stopni");
-    return [angleRadians < 0 ? angleRadians + Math.PI : angleRadians, angleDegrees];
-};
 
 const test = (point1, point2) => {
     // Oblicz wektor różnicy między dwoma punktami
@@ -93,23 +64,7 @@ for (let currentPointIndex = 0; currentPointIndex < points.length; currentPointI
     const previousPoint = currentPointIndex !== 0 ? currentPointIndex - 1 : points.length - 1;
 
     const currentPoint = points[currentPointIndex];
-
-    // const angle = calculateAngle(
-    //     new THREE.Vector3(points[nextPoint].x, points[nextPoint].y),
-    //     new THREE.Vector3(currentPoint.x, currentPoint.y),
-    //     new THREE.Vector3(points[previousPoint].x, points[previousPoint].y)
-    // );
-    // console.log(angle);
-    //
-    // const direction = checkAngle(
-    //     new THREE.Vector2(points[previousPoint].x, points[previousPoint].y),
-    //     new THREE.Vector2(points[nextPoint].x, points[nextPoint].y)
-    // );
-    // console.log(direction);
-    // const dir = test(currentPoint, points[nextPoint]);
-    // const hmm = test2(test(currentPoint, points[nextPoint]));
-
-    // 0, PI/2
+    
     shape.absarc(
         currentPoint.x,
         currentPoint.y,
