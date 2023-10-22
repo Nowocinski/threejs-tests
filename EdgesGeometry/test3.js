c2ae8cfa05e5e579e907efaa79596d6f4bc8b6a8
rafal.nowocinski <vito24222@gmail.com>
2023-10-22 17:15:02 +0200

    Create test3.js

---
+++ EdgesGeometry/test3.js
@@ -0,0 +1,96 @@
+// https://jsfiddle.net/prisoner849/pjb3cdm8/
+
+console.clear();
+
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(60, innerWidth / innerHeight, 1, 1000);
+camera.position.set(0, 8, 13).setLength(10);
+camera.lookAt(scene.position);
+let renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize(innerWidth, innerHeight);
+renderer.setClearColor(0x404040);
+document.body.appendChild(renderer.domElement);
+
+let controls = new OrbitControls(camera, renderer.domElement);
+
+let light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.setScalar(1);
+scene.add(light, new THREE.AmbientLight(0xffffff, 0.5));
+
+let shape = new THREE.Shape();
+let radius = 0.1;
+
+const getDirectionPoint = (point1, point2) => {
+    // Oblicz wektor różnicy między dwoma punktami
+    const directionVector = point2.clone().sub(point1);
+
+    // Oblicz kierunek prostopadły poprzez obrót o 90 stopni
+    const perpendicularDirection = new THREE.Vector2(-directionVector.y, directionVector.x);
+
+    // Normalizuj wektor, jeśli chcesz jedynie kierunek
+    perpendicularDirection.normalize();
+
+    console.log(perpendicularDirection); // Kierunek prostopadły
+
+    return perpendicularDirection;
+};
+
+const getDirectionAngle = (vector2) => {
+    // Oblicz kąt w radianach
+    const radians = Math.atan2(vector2.y, vector2.x);
+
+    // Przekształć kąt z radianów na stopnie
+    const degrees = (radians * 180) / Math.PI;
+
+    console.log(`Kąt: ${degrees} stopni`);
+
+    return radians;
+};
+
+const points = [
+    new THREE.Vector2(4, 2),
+    new THREE.Vector2(2, 1),
+    new THREE.Vector2(-2, 2),
+    new THREE.Vector2(-4, -2),
+    new THREE.Vector2(2, -2)];
+
+for (let currentPointIndex = 0; currentPointIndex < points.length; currentPointIndex++) {
+    const nextPoint = currentPointIndex !== points.length - 1 ? currentPointIndex + 1 : 0;
+    const previousPoint = currentPointIndex !== 0 ? currentPointIndex - 1 : points.length - 1;
+
+    const currentPoint = points[currentPointIndex];
+
+    shape.absarc(
+        currentPoint.x,
+        currentPoint.y,
+        /*radius*/Math.random(),
+        getDirectionAngle(getDirectionPoint( points[previousPoint], currentPoint)) + Math.PI,
+        getDirectionAngle(getDirectionPoint(currentPoint, points[nextPoint])) + Math.PI);
+}
+
+const loader = new THREE.CubeTextureLoader();
+loader.setPath( 'https://threejs.org/examples/textures/cube/pisa/' );
+
+const textureCube = loader.load( [
+    'px.png', 'nx.png',
+    'py.png', 'ny.png',
+    'pz.png', 'nz.png'
+] );
+
+let g = new THREE.ExtrudeGeometry(shape, {
+    depth: 3,
+    bevelEnabled: false
+});
+g.center();
+g.rotateX(Math.PI * -0.5);
+
+let m = new THREE.MeshStandardMaterial({color: "aqua", envMap: textureCube, metalness: 1, roughness: 0.25, side: THREE.DoubleSide});
+let o = new THREE.Mesh(g, m);
+scene.add(o);
+
+renderer.setAnimationLoop( _ => {
+    renderer.render(scene, camera);
+})
